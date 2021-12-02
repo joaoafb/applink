60ffe39e5f7eea000fe7f42d8ad8740434171201
joaoafb <82537300+joaoafb@users.noreply.github.com>
2021-12-02 18:02:16 -0300

    Add files via upload

---
+++ assents/js/menu.js
@@ -0,0 +1,47 @@
+function config(){
+    document.getElementById("divlogin").style.display = "none"
+    document.getElementById("divreoferta").style.display = "none"
+    document.getElementById("box2").style.display = "none"
+    document.getElementById("box3").style.display = "none"
+    document.getElementById("box1").style.display = "none"
+    document.getElementById("divhome").style.display = "none"
+    document.getElementById("divconfig").style.display = "block"
+    document.getElementById("divbemvindo").style.display = "none"
+    
+}
+
+
+function home(){
+    document.getElementById("divlogin").style.display = "none"
+    document.getElementById("divreoferta").style.display = "none"
+    document.getElementById("box2").style.display = "none"
+    document.getElementById("box3").style.display = "none"
+    document.getElementById("box1").style.display = "none"
+    document.getElementById("divhome").style.display = "block"
+    document.getElementById("divconfig").style.display = "none"
+    document.getElementById("divbemvindo").style.display = "none"
+}
+
+
+function reoferta(){
+    document.getElementById("divlogin").style.display = "none"
+    document.getElementById("divreoferta").style.display = "block"
+    document.getElementById("box2").style.display = "none"
+    document.getElementById("box3").style.display = "none"
+    document.getElementById("box1").style.display = "none"
+    document.getElementById("divhome").style.display = "none"
+    document.getElementById("divconfig").style.display = "none"
+    document.getElementById("divbemvindo").style.display = "none"
+}
+
+
+function login(){
+    document.getElementById("divlogin").style.display = "block"
+    document.getElementById("divreoferta").style.display = "none"
+    document.getElementById("box2").style.display = "none"
+    document.getElementById("box3").style.display = "none"
+    document.getElementById("box1").style.display = "none"
+    document.getElementById("divhome").style.display = "none"
+    document.getElementById("divconfig").style.display = "none"
+    document.getElementById("divbemvindo").style.display = "none"
+}
